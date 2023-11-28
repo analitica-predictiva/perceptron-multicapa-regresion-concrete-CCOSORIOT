5e28453af35c30fca9efdbd778302be3a434689d
CCOSORIOT <127028350+CCOSORIOT@users.noreply.github.com>
2023-11-28 18:36:00 -0500

    Pregunta 5

--- preguntas.py
+++ preguntas.py
@@ -59,6 +59,7 @@ def pregunta_02():
     # Retorne `X_train`, `X_test`, `y_train` y `y_test`
     return x_train, x_test, y_train, y_test
 
+
 def pregunta_03():
     """
     Construcción del pipeline
@@ -113,13 +114,13 @@ def pregunta_04():
     #   * Use parada temprana
 
     param_grid = {
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
-        ___: ____,  
+        "mlpregressor__hidden_layer_sizes": list(range(1,9)),  
+        "mlpregressor__activation": ["relu"],  
+        "mlpregressor__learning_rate": ["adaptive"],  
+        "mlpregressor__momentum": [0.7,0.8,0.9],  
+        "mlpregressor__learning_rate_init": [0.01,0.05,0.1],  
+        "mlpregressor__max_iter": [5000],  
+        "mlpregressor__early_stopping": [True],  
     }
 
     estimator = pregunta_03()
@@ -131,8 +132,8 @@ def pregunta_04():
     gridsearchcv = GridSearchCV(
         estimator=estimator,
         param_grid=param_grid,
-        ___ = ____  
-        ___ = ____  
+        cv = 5,
+        scoring = "r2"
     )
 
     return gridsearchcv
@@ -144,7 +145,7 @@ def pregunta_05():
     """
 
     # Importe mean_squared_error
-    from ____ import ____
+    from sklearn.metrics import mean_squared_error
 
     # Cargue las variables.
     x_train, x_test, y_train, y_test = pregunta_02()
@@ -153,20 +154,20 @@ def pregunta_05():
     estimator = pregunta_04()
 
     # Entrene el estimador
-    estimator.fit(x_train, y_train)  #
+    estimator.fit(x_train, y_train)
 
     # Pronostique para las muestras de entrenamiento y validacion
-    y_trian_pred = ____.____(____)  
-    y_test_pred = ____.____(____)  
+    y_train_pred = estimator.predict(x_train)  
+    y_test_pred = estimator.predict(x_test)   
 
     # Calcule el error cuadrático medio de las muestras
-    mse_train = ____(  
-        ___,  
-        ___,  
+    mse_train = mean_squared_error(  
+        y_train,  
+        y_train_pred,  
     )
-    mse_test = ____(  
-        ___,  
-        ___,  
+    mse_test = mean_squared_error(  
+        y_test,  
+        y_test_pred,  
     )
 
     # Retorne el mse de entrenamiento y prueba
